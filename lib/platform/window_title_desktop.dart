aae584aabdb8003e04dce9d26b4551a94cbc3474
lixingyu41q <415758212@qq.com>
2026-04-17 17:37:48 +0800

    Set minimum desktop window size

--- lib/platform/window_title_desktop.dart
+++ lib/platform/window_title_desktop.dart
@@ -1,15 +1,25 @@
 import 'dart:io';
+import 'dart:ui';
 
 import 'package:window_manager/window_manager.dart';
 
 bool get _isDesktop =>
     Platform.isWindows || Platform.isMacOS || Platform.isLinux;
 
+const Size _minimumWindowSize = Size(1120, 720);
+
 Future<void> initializeWindowTitlePlatform() async {
   if (!_isDesktop) {
     return;
   }
   await windowManager.ensureInitialized();
+  await windowManager.setMinimumSize(_minimumWindowSize);
+
+  final currentSize = await windowManager.getSize();
+  if (currentSize.width < _minimumWindowSize.width ||
+      currentSize.height < _minimumWindowSize.height) {
+    await windowManager.setSize(_minimumWindowSize);
+  }
 }
 
 Future<void> setAppWindowTitlePlatform(String title) async {
